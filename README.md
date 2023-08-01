ca698ed233bda7308ac64fcc1b00bfe115fd269a
WANG Xuerui <git@xen0n.name>
2023-08-01 21:03:39 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 龍眼 (Longan)
+
+Because this fruit sounds a bit like “龙研” -- "Loong Research".
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define __unused __attribute__((unused))
+
+int main(int argc __unused, const char *const argv[] __unused)
+{
+    return 0;
+}
